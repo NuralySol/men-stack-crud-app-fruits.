c5e2e343353d1148440f007ae446652470c969ca
NuralySol <soltonbekov@gmail.com>
2024-07-30 16:49:24 -0400

    final commit

--- server.js
+++ server.js
@@ -1,21 +1,32 @@
-// Here is where we import modules
-// We begin by loading Express
-
-//! const express = require("express"); old way of doing things
-
 import dotenv from "dotenv";
-dotenv.config()
+dotenv.config();
 import mongoose from "mongoose";
-
-// Import fruit from models 
+import methodOverride from "method-override";
+import logger from "morgan";
 import Fruit from "./models/fruit.js";
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
-import express from "express"; // new way of doing express import express
+// Get the directory name of the current module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
-app.use(express.urlencoded({ extended: false })); //! read information from the body of request
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(logger("dev"));
+app.use(express.static(path.join(__dirname, "public")));
 
-mongoose.connect(process.env.MONGODB_URI) //! connect to MONGODB_URI
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connected to MongoDB");
+}).catch(err => {
+    console.error("Failed to connect to MongoDB", err);
+});
 
 // GET 
 app.get("/", async (req, res) => {
@@ -28,34 +39,45 @@ app.get("/fruits", async (req, res) => {
     res.render("fruits/index.ejs", { fruits: allFruits });
 });
 
-
 // GET /fruits/new
 app.get("/fruits/new", async (req, res) => {
     res.render("fruits/new.ejs");
 });
-// GET
+
+// GET /fruits/:fruitId
 app.get("/fruits/:fruitId", async (req, res) => {
     const foundFruit = await Fruit.findById(req.params.fruitId);
     res.render("fruits/show.ejs", { fruit: foundFruit });
 });
 
-// POST 
-app.post("/fruits", async (req, res) => {
-    console.log(req.body);
-    // talk to the DB - through model
-    if (req.body.isReadyToEat === "on") {
-        req.body.isReadyToEat = true;
-    } else {
-        req.body.isReadyToEat = false;
-    }
-    await Fruit.create(req.body); // pushes the Schema to DB mongoose
-    res.redirect("/fruits");      // redirect the UI/UX back to /fruits url and body index
+// GET /fruits/:fruitId/edit
+app.get("/fruits/:fruitId/edit", async (req, res) => {
+    const foundFruit = await Fruit.findById(req.params.fruitId);
+    res.render("fruits/edit.ejs", { fruit: foundFruit });
 });
 
+// POST 
+app.post("/fruits", async (req, res) => {
+    req.body.isReadyToEat = req.body.isReadyToEat === "on";
+    await Fruit.create(req.body);
+    res.redirect("/fruits");
+});
+
+// DELETE /fruits/:fruitId
+app.delete("/fruits/:fruitId", async (req, res) => {
+    await Fruit.findByIdAndDelete(req.params.fruitId);
+    res.redirect("/fruits");
+});
+
+// PUT /fruits/:fruitId
+app.put("/fruits/:fruitId", async (req, res) => {
+    req.body.isReadyToEat = req.body.isReadyToEat === "on";
+    await Fruit.findByIdAndUpdate(req.params.fruitId, req.body);
+    res.redirect(`/fruits/${req.params.fruitId}`);
+});
 
 // connection listener to MONGODB and listening to port 3000
 mongoose.connection.on("connected", () => {
-    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
     app.listen(3000, () => {
         console.log("Listening on port 3000");
     });
